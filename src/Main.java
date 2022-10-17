6ae5fefc6159ba68010e0159bd761d29fca4ce68
ihasicic1 <ihasicic1@etf.unsa.ba>
2022-10-17 15:35:56 +0200

    Ilhan

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int n = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Unesite broj n: ");
+        n = scanner.nextInt();
+
+    }
+}
